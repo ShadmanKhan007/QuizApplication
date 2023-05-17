d0a847102d0076c8c525416516ec8f28dd7eeabe
Shadman Khan <shaad2209@gmail.com>
2023-05-17 22:50:06 +0530

    quizapp

---
+++ src/components/Quiz/QuizData.jsx
@@ -0,0 +1,53 @@
+export const QuizData = [
+    {
+        question: "What is Reactjs",
+        options:[ "framework", "language", "library", "compiler"],
+        answer: 2
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "ReactJs is created by",
+        options: ["Elon Musk", "Mark Zukerberg", "Jordan Walke", "none of the above"],
+        answer:3
+    },
+    {
+        question: "Today is Monday After 63 days is",
+        options:[ "Tuesday", "Monday", "Sunday", "Saturday"],
+        answer: 2
+    },
+    {
+        question: "Today is Tuesday After 64 days is",
+        options:[ "Tuesday", "Monday", "Wednesday", "Saturday"],
+        answer: 3
+    },
+    {
+        question: "Capital of India?",
+        options: ["New Delhi", "Mumbai", "Noida", "kolkata"],
+        answer: 1
+    },
+    {
+        question: "Capital Of England",
+        options: ["Scotland", "London", "Paris", "NewYork"],
+        answer:2
+    },
+    {
+        question: "National Bird Of India?",
+        options: ["Peocock", "Eagle", "Sparrow", "Rabbit"],
+        answer: 1
+    },
+    {
+        question: "National Animal Of India",
+        options: ["Lion", "Elephant", "Tiger", "Bear"],
+        answer:3
+    },
+    
+];
